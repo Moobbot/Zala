49a26ed865239cc039b53584540924c9ab4eee1d
Moobbot <ngotam24082001@gmai.com>
2022-02-14 13:37:22 +0700

    new & shoplist update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 (function($) {
     "use strict"
 
-
     /* 1. Proloder */
     $(window).on('load', function() {
         $('#preloader-active').delay(450).fadeOut('slow');
@@ -34,64 +33,6 @@
     $("[data-background]").each(function() {
         $(this).css("background", "url(" + $(this).attr("data-background") + ")" + "0 / cover no-repeat")
     });
-
-    /* 10. WOW active */
-    new WOW().init();
-
-    // 11. ---- Mailchimp js --------//  
-    function mailChimp() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-    mailChimp();
-
-
-    // 12 Pop Up Img
-    var popUp = $('.single_gallery_part, .img-pop-up');
-    if (popUp.length) {
-        popUp.magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    }
-    // 12 Pop Up Video
-    var popUp = $('.popup-video');
-    if (popUp.length) {
-        popUp.magnificPopup({
-            type: 'iframe'
-        });
-    }
-
-    /* 13. counterUp*/
-    $('.counter').counterUp({
-        delay: 10,
-        time: 3000
-    });
-
-    /* 14. Datepicker */
-    $('#datepicker1').datepicker();
-
-    // 15. Time Picker
-    $('#timepicker').timepicker();
-
-    //16. Overlay
-    $(".snake").snakeify({
-        speed: 200
-    });
-
-
-    //17.  Progress barfiller
-
-    $('#bar1').barfiller();
-    $('#bar2').barfiller();
-    $('#bar3').barfiller();
-    $('#bar4').barfiller();
-    $('#bar5').barfiller();
-    $('#bar6').barfiller();
-
-
-
 
     // Modal Activation
     $('.search-switch').on('click', function() {
